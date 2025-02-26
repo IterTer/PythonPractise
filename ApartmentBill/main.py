4f1bfb3ea1c53bd9be9c9178b7caa9363b696ecc
TT <tii2tuo@gmail.com>
2025-02-26 14:02:17 +0200

    Add CLI

--- ApartmentBill/main.py
+++ ApartmentBill/main.py
@@ -66,12 +66,20 @@ class PdfReport:
         pdf.output(self.filename)
         webbrowser.open(self.filename)
 
-the_bill = Bill(amount=120, period="March 2021")
-john = Flatmate(name="John", days_in_house=20)
-mary = Flatmate(name="Mary", days_in_house=25)
+amount = float(input("Hey, enter the billing amount: "))
+period = input("What is the billing period (E.g. December 2024): ")
 
-print("John pays:", john.pays(the_bill, flatmate2=mary))
-print("Mary pays:", mary.pays(the_bill, flatmate2=john))
+name1 = input("What is your name? ")
+days_in_house1 = int(input(f"How many days did {name1} stay in the apartment during {period}: "))
+name2 = input("What is the name of the other flatmate? ")
+days_in_house2 = int(input(f"How many days did {name2} stay in the apartment during {period}: "))
 
-pdf_report = PdfReport(filename='Report1.pdf')
-pdf_report.generate(flatmate1=john, flatmate2=mary, bill=the_bill)
+the_bill = Bill(amount, period)
+flatmate1 = Flatmate(name1, days_in_house1)
+flatmate2 = Flatmate(name2, days_in_house2)
+
+print(f"{name1}:", flatmate1.pays(the_bill, flatmate2))
+print(f"{name2}:", flatmate2.pays(the_bill, flatmate1))
+
+pdf_report = PdfReport(filename=f"{the_bill.period}.pdf")
+pdf_report.generate(flatmate1, flatmate2, the_bill)
